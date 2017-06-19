c96df578fec962eb2c56aaec31e5c77a7a101a96
adityanshastry <adityashastry1992@gmail.com>
2017-06-19 15:38:47 -0400

    removing repotitions

--- Frequency_Summarization/Constants.py
+++
@@ -1,9 +0,0 @@
-from nltk.corpus import stopwords
-from string import punctuation
-
-NOUN_POS_TAGS = ("NN", "NNP", "NNPS", "NNS")
-STOP_WORDS = set(stopwords.words('english'))
-PUNCTUATIONS = set(unicode(punctuation))
-READ_MODE = "r"
-WRITE_MODE = "w"
-UTF_8 = "utf-8-sig"
