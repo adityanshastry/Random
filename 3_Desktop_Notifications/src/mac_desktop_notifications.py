017bef48c5e0db0fe21f1c2b49eeb7dd04e54792
adityanshastry <adityashastry1992@gmail.com>
2017-08-11 11:58:31 -0400

    renaming for easier file traversal

--- 3_Desktop_Notifications/src/mac_desktop_notifications.py
+++ 3_Desktop_Notifications/src/mac_desktop_notifications.py
@@ -9,29 +9,32 @@ def notify(title, message):
     os.system('terminal-notifier {}'.format(' '.join([notification_title, notification_message])))
 
 
-def notify_file_lines(files, check_files):
+def notify_file_lines(files, check_files, monitor_time):
 
     line_counts = [0 for _ in xrange(len(files))]
     continue_notifications = True
+    equal_count = 0
 
-    while continue_notifications:
+    while equal_count <= 2:
         message = []
         for data_file_index, data_file in enumerate(files):
             line_count = int(subprocess.check_output(['wc', '-l', data_file]).strip().split()[0])
+            message.append(str(line_count))
             if data_file_index+1 in check_files and line_count == line_counts[data_file_index]:
-                continue_notifications = False
+                equal_count += 1
             else:
                 line_counts[data_file_index] = line_count
-                message.append(str(line_count))
+
+        print message
         notify("Extraction Progress", ", ".join(message))
 
-        time.sleep(60)
+        time.sleep(monitor_time)
 
 
 def main():
     notify_file_lines(["/Users/BatComp/Desktop/UMass/IESL/Code/watr-works/arxiv-sample_1.txt",
-                       "/Users/BatComp/Desktop/UMass/IESL/Code/watr-works/arxiv-exceptions_2.txt"], [1])
-    # notify("g", "qw")
+                       "/Users/BatComp/Desktop/UMass/IESL/Code/watr-works/arxiv-exceptions_2.txt"], [1], 60)
+
 
 if __name__ == '__main__':
     main()
